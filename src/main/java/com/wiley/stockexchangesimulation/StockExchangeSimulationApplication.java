b35c9389215c2a0953bcdf851117f6bd36cfd815
ishanrtripathi <ishanrtripathi786@gmail.com>
2020-06-13 20:47:16 +0530

    Initial commit

---
+++ src/main/java/com/wiley/stockexchangesimulation/StockExchangeSimulationApplication.java
@@ -0,0 +1,13 @@
+package com.wiley.stockexchangesimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockExchangeSimulationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockExchangeSimulationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wiley/stockexchangesimulation/StockExchangeSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiley.stockexchangesimulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockExchangeSimulationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
