6149f3e12fa04c8b8e4a8b3a750da89b15e1ea26
ishanrtripathi <ishanrtripathi786@gmail.com>
2020-06-13 22:24:19 +0530

    Ishan Commit

--- src/main/java/com/wiley/stockexchangesimulation/StockExchangeSimulationApplication.java
+++ src/main/java/com/wiley/stockexchangesimulation/StockExchangeSimulationApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StockExchangeSimulationApplication {
 
     public static void main(String[] args) {
+
+        System.out.println("Yyaay");
         SpringApplication.run(StockExchangeSimulationApplication.class, args);
     }
 
